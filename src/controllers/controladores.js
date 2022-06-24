b5ea4976842322b7f7e5fbf7eb460e7f6485ffc4
svelezpernett <89142690+svelezpernett@users.noreply.github.com>
2022-06-24 12:34:50 -0500

    se crea interfaz index

---
+++ src/controllers/controladores.js
@@ -0,0 +1,17 @@
+//entradas== variables
+let nombre="juan"
+
+
+
+//controlando etiquetas
+//1. necestio almacenar una etiqueta en una variable 
+let titulo=document.getElementById("titulo")
+let imagen=document.getElementById("foto")
+
+//2. identifico que quiero controlar
+//2.1 cambio el texto
+titulo.textContent="chao"
+
+//2.2 cambiando la fuente src
+
+imagen.src="https://placeimg.com/640/480/any"
